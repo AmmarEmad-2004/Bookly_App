5c9a761615397054809985a6f07c370cbb11067c
AmmarEmad-2004 <za0662003@email.com>
2025-03-03 18:00:43 +0200

    add CustomListViewItem widget and update assets data for image usage

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,31 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: [
         CustomAppBar(),
+        CustomListViewItem(),
       ],
+    );
+  }
+}
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.25,
+      child: AspectRatio(
+        aspectRatio: 2.9 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.red,
+            image: DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
