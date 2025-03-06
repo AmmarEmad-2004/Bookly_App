4f9428a73ec8fbf016f5fd7f663c8d28e4f57ad1
AmmarEmad-2004 <za0662003@email.com>
2025-03-06 22:54:36 +0200

    add BookDetailsViewBody and related widgets for improved book details display

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsView extends StatelessWidget {
@@ -5,6 +6,67 @@ class BookDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
+
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          CustomDetailsViewAppBar(),
+          BookDetailsItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomDetailsViewAppBar extends StatelessWidget {
+  const CustomDetailsViewAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.close),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart_checkout_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BookDetailsItem extends StatelessWidget {
+  const BookDetailsItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.19),
+          child: CustomBookItem(),
+        )
+      ],
+    );
   }
 }
